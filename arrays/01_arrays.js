aa07a69aa12dfb4ce3c7a13e4d9c1157d5fd7914
umesh <your-email@example.com>
2025-03-31 18:12:58 +0530

    commited

---
+++ arrays/01_arrays.js
@@ -0,0 +1,4 @@
+const myArr = [1,2,3,4,true,"umesh"]
+// arrays can store different datatypes
+// arrays are resizable
+// during copy of arrays it create shallow copy(same reference as the original), not deep copies(not same referance as original)
